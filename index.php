084408101b375d5aa10c861fd83faa3b693ad995
foxydepiraat <foxydepiraat@gmail.com>
2022-05-17 13:19:36 +0200

    begin gemaakt

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<?php
+    include("DBfestival.php");
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div id="loginMenu">
+        <form method="POST">
+        <input type=""/>
+        </form>
+    </div>
 </body>
 </html>
---
+++ overzicht.php
@@ -0,0 +1,13 @@
+<?php
+    include("DBfestival.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>overzicht</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
