4012b508b884a03cdd7c34154a748fe4262199fd
foxydepiraat <foxydepiraat@gmail.com>
2022-05-17 10:49:02 +0200

    database geconnect

--- DBfestival.php
+++ DBfestival.php
@@ -1,3 +1,15 @@
 <?php
+//database van de festival
+$host="localhost";
+$dbname="dancefestival";
+$user="root";
+$password="";
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname" ,$user, $password);
+    }catch(PDOException $ex){
+        echo "verbinding mislukt .</br>";
+    }
+
 
 ?>
