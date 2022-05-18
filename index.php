08dddd7401d068d5cecf0db3cde2286d51d0482d
foxydepiraat <foxydepiraat@gmail.com>
2022-05-18 08:39:05 +0200

    inlog en regristreer

--- index.php
+++ index.php
@@ -11,8 +11,16 @@
 <body>
     <div id="loginMenu">
         <form method="POST">
-        <input type=""/>
+        <input type="submit" name="btninloggen" value="INLOGGEN"/>
+        
         </form>
     </div>
+    <div id="regristreerMenu">
+        <form method="POST">
+        <input type="submit" name="btnRegristreren" value="REGRISTREREN"/>
+        </form>
+    </div>
+    
+    
 </body>
 </html>
